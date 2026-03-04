22a24da257cac3744ef852692e44eee8a33b741f
Madhumitha212 <madhuravi324@gmail.com>
2026-03-04 21:53:00 +0530

    add: [Madhu] added a utils file to connect to RDS

---
+++ aws_storage_with_rds/scripts/db_utils.py
@@ -0,0 +1,20 @@
+from .insertion import *
+
+def execute_query(query, params=(), fetch=False):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(query, params)
+
+    if fetch:
+        result = cursor.fetchall()
+        row_count = len(result)
+    else:
+        conn.commit()
+        row_count = cursor.rowcount
+        result = None
+
+    cursor.close()
+    conn.close()
+
+    return result, row_count
