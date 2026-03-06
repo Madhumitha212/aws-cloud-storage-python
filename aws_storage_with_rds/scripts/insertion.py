774b1b199c2f079b9948d6607f3792e3bc2ddae1
Madhumitha212 <madhuravi324@gmail.com>
2026-03-06 06:22:14 +0530

    update: [Madhu] - updated program for inserting data

--- aws_storage_with_rds/scripts/insertion.py
+++ aws_storage_with_rds/scripts/insertion.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from io import StringIO
 from config.connection import *
-from config.s3_config import *
+import config.s3_config as s3_config
+import pyodbc  # assuming you use pyodbc
 
 def create_table_if_not_exists(conn):
-
     cursor = conn.cursor()
     create_table_query = """
     IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'transactions')
@@ -25,23 +26,33 @@ def create_table_if_not_exists(conn):
     cursor.execute(create_table_query)
     conn.commit()
 
-def insert_data(conn):
-    # Load CSV
-    df = pd.read_csv(LOCAL_PROCESS_DOWNLOAD_PATH)
+
+def read_csv_from_s3(file_key):
+    s3 = s3_config.get_s3_client()
+    response = s3.get_object(
+        Bucket=s3_config.S3_BUCKET_NAME,
+        Key=file_key
+    )
+    csv_data = response["Body"].read().decode("utf-8")
+    df = pd.read_csv(StringIO(csv_data))
+    return df
+
+
+def insert_data(conn, file_key):
+    df = read_csv_from_s3(file_key)
 
     required_columns = [
-    "transaction_id",
-    "transaction_date",
-    "customer_id",
-    "product_id",
-    "region",
-    "quantity",
-    "price_per_unit",
-    "total_amount",
-    "discount_percent",
-    "final_amount"
+        "transaction_id",
+        "transaction_date",
+        "customer_id",
+        "product_id",
+        "region",
+        "quantity",
+        "price_per_unit",
+        "total_amount",
+        "discount_percent",
+        "final_amount"
     ]
-
     df = df[required_columns]
     cursor = conn.cursor()
 
@@ -55,16 +66,19 @@ def insert_data(conn):
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
 
-    cursor.fast_executemany = True
-    cursor.executemany(insert_query, df.values.tolist())
+    try:
+        cursor.fast_executemany = True
+        cursor.executemany(insert_query, df.values.tolist())
+        conn.commit()
+        print("Data inserted successfully!")
+    except pyodbc.IntegrityError as e:
+        # This error occurs if transaction_id already exists
+        print("Error: Some rows already exist in the database. Aborting insert.")
+        raise e  # re-raise the exception to stop execution
 
-    conn.commit()
-
-    print("Data inserted successfully!")
 
 if __name__ == "__main__":
-
     conn = get_connection()
     create_table_if_not_exists(conn)
-    insert_data(conn)
+    insert_data(conn, "processed/cleaned_sales.csv")
     conn.close()
