82b22c0657f15c8e384dd88152a989f438c49f72
Madhumitha212 <madhuravi324@gmail.com>
2026-03-06 06:20:58 +0530

    update: [Madhu] - updated program by if already data inserted raise exception

--- aws_storage_with_rds/scripts/export_to_csv.py
+++ aws_storage_with_rds/scripts/export_to_csv.py
@@ -1,48 +1,22 @@
-import os
-import pandas as pd
-from config.connection import *
+import csv
+from io import StringIO
+from config.s3_config import get_s3_client, S3_BUCKET_NAME   # adjust import if needed
 
 
-def export_to_csv():
-    conn = get_connection()
-    cursor = conn.cursor()
+def upload_query_results_to_s3(file_name, rows, headers):
+    
+    s3 = get_s3_client()
 
-    # Run query
-    cursor.execute("SELECT * FROM transactions")
-    rows = cursor.fetchall()
-    print("Rows fetched:", len(rows))
+    csv_buffer = StringIO()
+    writer = csv.writer(csv_buffer)
 
-    # Get column names directly from cursor
-    columns = [desc[0] for desc in cursor.description]
+    writer.writerow(headers)
+    writer.writerows(rows)
 
-    # Convert to DataFrame
-    df = pd.DataFrame.from_records(rows, columns=columns)
+    s3.put_object(
+        Bucket=S3_BUCKET_NAME,
+        Key=file_name,
+        Body=csv_buffer.getvalue()
+    )
 
-    # Ensure directory exists
-    output_dir = "/mnt/d/aws_cloud_storage/aws_storage_with_rds/downloads"
-    os.makedirs(output_dir, exist_ok=True)
-    output_path = os.path.join(output_dir, "exported_results.csv")
-
-    # Save to CSV
-    df.to_csv(output_path, index=False)
-    print(f"Data exported successfully! Rows: {len(df)} File: {output_path}")
-
-    cursor.close()
-    conn.close()
-
-if __name__ == "__main__":
-    export_to_csv()
-
-# 3. Explain different methods for bulk import/export
-
-# 1. Using SQL Server Management Studio (SSMS) for SQL Server: 
-#   This tool provides a graphical interface for importing and exporting 
-#   data using the Import and Export Wizard.
-
-# 2. Using BCP (Bulk Copy Program): This is a command-line tool that allows 
-#   for fast import and export of data between SQL Server and other data 
-#   sources.Very fast handles millions of data.
-
-# 3. Using Python libraries like pandas and pyodbc: This method allows for 
-#   programmatic control over the import and export process, making it 
-#   suitable for automation and integration into data pipelines.
+    print(f"{file_name} uploaded successfully to {S3_BUCKET_NAME}")
